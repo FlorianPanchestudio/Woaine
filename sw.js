0deaa7e388b9a2d948853e8b477629a7a2fbb900
FlorianPanchestudio <florian.b@panachestudio.fr>
2026-03-20 09:00:03 +0100

    Ajout de l'app Woaine PWA

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'woaine-v1';
+const FILES = [
+  '/woaine/',
+  '/woaine/index.html',
+  '/woaine/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
